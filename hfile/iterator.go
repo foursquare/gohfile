b08b9d12af7b42f18a6a110a646ac877b4ae50ae
Daniel Harrison <dan@foursquare.com>
2015-08-12 14:39:03 +0000

    Iterator support

---
+++ hfile/iterator.go
@@ -0,0 +1,50 @@
+// Copyright (C) 2014 Daniel Harrison
+
+package hfile
+
+import "encoding/binary"
+
+type Iterator struct {
+	hfile          *Reader
+	dataBlockIndex int
+	key            []byte
+	value          []byte
+}
+
+func (hfile *Reader) NewIterator() *Iterator {
+	it := Iterator{hfile, 0, nil, nil}
+	return &it
+}
+
+func (it *Iterator) Next() bool {
+	if it.dataBlockIndex >= len(it.hfile.dataIndex.dataBlocks) {
+		return false
+	}
+	dataBlock := it.hfile.dataIndex.dataBlocks[it.dataBlockIndex]
+	if dataBlock.buf.Len() <= 0 {
+		it.dataBlockIndex += 1
+		if it.dataBlockIndex >= len(it.hfile.dataIndex.dataBlocks) {
+			return false
+		}
+		dataBlock := it.hfile.dataIndex.dataBlocks[it.dataBlockIndex]
+		dataBlock.reset()
+		return it.Next()
+	}
+
+	var keyLen, valLen uint32
+	binary.Read(dataBlock.buf, binary.BigEndian, &keyLen)
+	binary.Read(dataBlock.buf, binary.BigEndian, &valLen)
+	it.key = make([]byte, keyLen)
+	it.value = make([]byte, valLen)
+	dataBlock.buf.Read(it.key)
+	dataBlock.buf.Read(it.value)
+	return true
+}
+
+func (it *Iterator) Key() []byte {
+	return it.key
+}
+
+func (it *Iterator) Value() []byte {
+	return it.value
+}
