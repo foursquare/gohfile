7f42474daeff0e953df83f4cf9371ba66fecd12a
David Taylor <tinystatemachine@gmail.com>
2015-08-14 19:13:32 +0000

    add lots of debug printing

--- hfile/scanner.go
+++ hfile/scanner.go
@@ -5,6 +5,7 @@ package hfile
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"sort"
@@ -29,12 +30,15 @@ func (s *Scanner) Reset() {
 
 func (s *Scanner) findBlock(key []byte) int {
 	remaining := len(s.reader.index) - s.idx - 1
+	log.Printf("[Scanner.findBlock] cur %d, remaining %d\n", s.idx, remaining)
 
 	if remaining <= 0 {
+		log.Println("[Scanner.findBlock] last block")
 		return s.idx // s.cur is the last block, so it is only choice.
 	}
 
 	if s.reader.index[s.idx+1].IsAfter(key) {
+		log.Println("[Scanner.findBlock] next block is past key")
 		return s.idx
 	}
 
@@ -60,14 +64,32 @@ func (s *Scanner) blockFor(key []byte) (*bytes.Reader, error, bool) {
 	}
 
 	if s.reader.index[s.idx].IsAfter(key) {
+		log.Printf("[Scanner.blockFor] curBlock after key %s (cur: %d, start: %s)\n",
+			hex.EncodeToString(key),
+			s.idx,
+			hex.EncodeToString(s.reader.index[s.idx].firstKeyBytes),
+		)
 		return nil, nil, false
 	}
 
 	idx := s.findBlock(key)
+	log.Printf("[Scanner.blockFor] findBlock key: %s. Picked %d (starts: %s). Cur: %d (starts: %s)\n",
+		hex.EncodeToString(key),
+		idx,
+		hex.EncodeToString(s.reader.index[idx].firstKeyBytes),
+		s.idx,
+		hex.EncodeToString(s.reader.index[s.idx].firstKeyBytes),
+	)
 
 	if idx != s.idx || s.buf == nil { // need to load a new block
 		data, err := s.reader.GetBlock(idx)
 		if err != nil {
+			log.Printf("[Scanner.blockFor] read err %s (key: %s, idx: %d, start: %s)\n",
+				err,
+				hex.EncodeToString(key),
+				idx,
+				hex.EncodeToString(s.reader.index[idx].firstKeyBytes),
+			)
 			return nil, err, false
 		}
 		s.idx = idx
@@ -81,6 +103,7 @@ func (s *Scanner) GetFirst(key []byte) ([]byte, error, bool) {
 	data, err, ok := s.blockFor(key)
 
 	if !ok {
+		log.Printf("[Scanner.GetFirst] No Block for key: %s (err: %s, found: %s)\n", hex.EncodeToString(key), err, ok)
 		return nil, err, ok
 	}
 
@@ -92,7 +115,7 @@ func (s *Scanner) GetAll(key []byte) ([][]byte, error) {
 	data, err, ok := s.blockFor(key)
 
 	if !ok {
-		log.Println("no block for key ", key)
+		log.Printf("[Scanner.GetAll] No Block for key: %s (err: %s, found: %s)\n", hex.EncodeToString(key), err, ok)
 		return nil, err
 	}
 
