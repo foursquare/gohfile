31b3525d4e67ee53f7309a22c3d431486794829a
David Taylor <tinystatemachine@gmail.com>
2015-08-28 05:07:49 +0000

    mock multi-value hfiles too

--- testdata.go
+++ testdata.go
@@ -14,22 +14,41 @@ func MockKeyInt(i int) []byte {
 func MockValueInt(i int) []byte {
 	return []byte(fmt.Sprintf("value-for-%d", i))
 }
+func MockMultiValueInt(i, k int) []byte {
+	return []byte(fmt.Sprintf("value-for-%d-%d", i, k))
+}
 
 func GenerateMockHfile(path string, keyCount, blockSize int, compress, verbose, progress bool) error {
 	w, err := NewLocalWriter(path, compress, blockSize, verbose)
 	if err != nil {
 		return err
 	}
-	return WriteMockIntPairs(w, keyCount, progress)
+	return WriteMockIntPairs(w, keyCount, progress, false)
 }
 
-func WriteMockIntPairs(w *Writer, keyCount int, progress bool) error {
+func GenerateMockMultiHfile(path string, keyCount, blockSize int, compress, verbose, progress bool) error {
+	w, err := NewLocalWriter(path, compress, blockSize, verbose)
+	if err != nil {
+		return err
+	}
+	return WriteMockIntPairs(w, keyCount, progress, true)
+}
+
+func WriteMockIntPairs(w *Writer, keyCount int, progress bool, multi bool) error {
 	for i := 0; i < keyCount; i++ {
 		if progress && i%10000 == 0 {
 			fmt.Printf("\r %d %.02f%%", i, (float64(i)*100.0)/float64(keyCount))
 		}
-		if err := w.Write(MockKeyInt(i), MockValueInt(i)); err != nil {
-			return err
+		if multi && i%2 == 1 {
+			for k := 0; k < 3; k++ {
+				if err := w.Write(MockKeyInt(i), MockMultiValueInt(i, k)); err != nil {
+					return err
+				}
+			}
+		} else {
+			if err := w.Write(MockKeyInt(i), MockValueInt(i)); err != nil {
+				return err
+			}
 		}
 	}
 
