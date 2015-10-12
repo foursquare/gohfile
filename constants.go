2ab24858cd5467114c95f7a00190f25a92bcf5a8
David Taylor <tinystatemachine@gmail.com>
2015-10-12 22:12:44 +0000

    moved to quiver

--- constants.go
+++
@@ -1,8 +0,0 @@
-package hfile
-
-var IndexMagic = []byte("IDXBLK)+")
-var DataMagic = []byte("DATABLK*")
-var TrailerMagic = []byte("TRABLK\"$")
-
-var CompressionNone = uint32(2)
-var CompressionSnappy = uint32(3)
